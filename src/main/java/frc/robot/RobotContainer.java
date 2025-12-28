b23e16d65c4f877a819e37e750976e489d65e477
blueGate3 <162855123+blueGate3@users.noreply.github.com>
2025-12-28 00:22:01 -0500

    odo works except flipped y axis odo - dumb wpilib coordinate system?

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,11 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructSubscriber;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -19,29 +23,29 @@ public class RobotContainer {
   private final Drivetrain m_drivetrain = new Drivetrain();
   private final CommandPS4Controller m_driverController = new CommandPS4Controller(0);
 
+  
+  private final StructSubscriber<Pose2d> poseSub = NetworkTableInstance.getDefault()
+  .getStructTopic("Robot/CurrentPose", Pose2d.struct).subscribe(new Pose2d());
+
   public RobotContainer() {
     configureBindings();
-
-    new RunCommand(
-      () -> m_drivetrain.drive(
+    m_drivetrain.setDefaultCommand(
+      Commands.run(
+        () -> m_drivetrain.drive(
           m_driverController.getRawAxis(0), 
           m_driverController.getRawAxis(1), 
-          m_driverController.getRawAxis(4), false), 
-        m_drivetrain);
-    
+          -m_driverController.getRawAxis(4), true), 
+        m_drivetrain)
+    );
   }
 
   private void configureBindings() {
-    drivetrain.setDefaultCommand(Commands.run(
-      () -> 
-      drivetrain.drive(
-        m_dc.getRawAxis(0), 
-        m_dc.getRawAxis(1), 
-        -m_dc.getRawAxis(4), true), 
-        drivetrain
-      ));
+  }
 
-      
+  public void repeat() {
+    SmartDashboard.putNumber("OdometryX", poseSub.get().getX());
+    SmartDashboard.putNumber("OdometryY", poseSub.get().getY()); //TODO for some reason this is backwards?
+    SmartDashboard.putNumber("OdometryRot", poseSub.get().getRotation().getDegrees());
   }
 
 }
